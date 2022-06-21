a1bb6f5bd50d607ade9b5bf4ae308ae66cf9f17e
srai98i <simran_rai982@outlook.com>
2022-06-21 14:24:01 +0100

    post router made

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/resources', router);
+app.use("/resources/titles", router)
 app.use("/resources/:id", router)
-app.use("/titles", router)
+app.use('/title', router);
+
 
 app.get('/', function (req, res) {
   res.json({
--- routes/routes.js
+++ routes/routes.js
@@ -16,11 +16,79 @@ router.get("/:id", async function (req, res) {
 }
 )
 
-router.get("/", async function (req, res) {
-  let allTitles = await query(`SELECT title FROM resources`);
-  res.json({success: true, payload: allTitles});
-}
-)
+router.post("/", async function(req, res){
+  let body = req.body
+  const sqlString = `INSERT INTO resources ( Url , Title, Type, Topic, Description ) VALUES ($1. $2, $3, $4, $5);`
+  const resourcesPost = await query(sqlString, [body.Url, body.Title, body.Type, body.Topic, body.Description])
+  res.json({success: true, payload: resourcesPost})
+})
+
+// router.post("/", function (req, res){
+//   let body = req.body
+//   users.push(body)
+//   const responseObject = { success: true, payload: body }
+//   res.json(responseObject)
+// })
+
+// use lines 19 and 20
+// line 21 is referencing an array= we want to interact with database
+// our line 22 is res.json and payload will be variable declared on line 21 
+
+// animalRouter.post("/", async function (req, res){
+//   let body = req.body
+//   const sqlString = `INSERT INTO animals ( animal_species , animal_name)
+//   VALUES ($1, $2);`
+//   const resPostAnimal = await query(sqlString, [body.animal_species, body.animal_name])
+//   let responseObject = {
+//     success: true,
+//     payload: resPostAnimal,
+//   };
+//   res.json(responseObject)
+//   console.log(responseObject)
+// })
+
+
+
+
+// router.put("/:id", function(req,res){
+//   let id = req.params.id
+//   let body = req.body
+//   let updatedUser = {}
+//   for (let i=0; i<users.length; i++) {
+//       if (Number(id) === users[i].id) {
+//           users[i] = body;
+//           updatedUser = users[i]
+//           break
+//       }
+//   }
+//   const response = { success: true, payload: updatedUser }
+//   res.json(response)
+// })
+
+// router.delete("/:id", function(req, res){
+//   let id = req.params.id;
+//   let deletedUser = {}
+//   for (let i = 0; i<users.length; i++) {
+//       if (Number(id) === users[i].id) { 
+//           deletedUser = users.splice(users[i].id)
+//           break
+//       }
+//   }
+//   let response = { success: true, payload: deletedUser }
+//   res.json(response)
+// })
+
+
+//plan 
+// post route
+// test it with postman 
+// put route 
+// test it with postman
+// patch route
+// test it with postman
+// delete route
+// test it with postman 
+
 
 
 export default router;
