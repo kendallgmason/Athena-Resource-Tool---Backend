61598189232f97866af38e8568b965e719dc8a00
srai98i <simran_rai982@outlook.com>
2022-06-22 19:36:47 +0100

    added tests for post and create routes

--- routes/routes.test.js
+++ routes/routes.test.js
@@ -21,12 +21,57 @@ it ("should return json content type", async () => {
       .expect('Content-Type', /json/)
       .expect(200)
 })
+it('should fetch a single post', async () => {
+    const postId = 1;
+    const res = await request(app).get(`/resources/${postId}`);
+    expect(res.statusCode).toEqual(200);
+  });
 
 })
 
+describe("POST /resources", () => {
+
+it('should update a post', async () => {
+    const res = await request(app)
+      .put('/resources/1')
+      .send({
+        "url": "updatedurl.com",
+        "title": "A Title",
+        "type": "A Type",
+        "topic": "A Topic",
+        "description": "Your description here."
+        });
+    console.log(res.body.payload)
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.payload).toEqual( [
+        {
+          id: 1,
+          url: 'updatedurl.com',
+          title: 'A Title',
+          type: 'A Type',
+          topic: 'A Topic',
+          description: 'Your description here.'
+        }
+      ])
+
+  });
 
 
+})
 
+describe("CREATE /resources", () => {
+it('should create a new post', async () => {
+    const res = await request(app)
+      .post('/resources')
+      .send({
+        "url": "newurl.com",
+        "title": "A Title",
+        "type": "A Type",
+        "topic": "A Topic",
+        "description": "Your description here."
+      });
+    expect(res.statusCode).toEqual(200);
 
-// checked that GET sends back 200
-//
+});
+
+})
