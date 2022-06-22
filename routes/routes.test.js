be607fcabf58d32f055f801064b330ac432fe06d
srai98i <simran_rai982@outlook.com>
2022-06-22 16:44:53 +0100

    some get tests working

--- routes/routes.test.js
+++ routes/routes.test.js
@@ -1,16 +1,32 @@
-import { request } from 'express';
+import  request  from 'supertest';
 import supertest from 'supertest';
 import app from '../app';
+
+
 
 describe("GET /resources", () => {
 
 
 it("should respond with a 200 status code", async () => {
-const response = await request(app).get("/resources")
-expect(response.statusCode).toBe
+    await request(app)
+    .get("/resources")
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(200);
+})
+it ("should return json content type", async () => {
+    await request(app)
+      .get('/resources')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+})
+
 })
 
 
-})
 
 
+
+// checked that GET sends back 200
+//
