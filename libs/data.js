9de009096ec4bdcb05e0fe2dd4266d97d8299993
shabana kauser <shabanakauser89@gmail.com>
2022-06-23 11:22:31 +0100

    added more resources made it up to 10

--- libs/data.js
+++ libs/data.js
@@ -31,6 +31,54 @@ const resources = [
         topic: 'React',
         description: "An article about what React.useReducer is used for, and when to use it instead ofReact.useState", 
         isFavourite: "false"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=w7ejDZ8SWv8",
+        title: 'React JS Crash Course',
+        type: 'Video',
+        topic: 'React',
+        description: "Get started with React in this crash course. We will be building a task tracker app and look at components, props, state, hooks, working with an API and more.",
+        isFavourite: "false"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=TNhaISOUy6Q",
+        title: '10 React Hooks Explained // Plus Build your own from Scratch',
+        type: 'Video',
+        topic: 'React',
+        description: "React hooks provide a highly-efficient was to tap into framework features and organize reactive logic. ",
+        isFavourite: "false"
+    },
+    {
+        url: "https://www.youtube.com/watch?v=USjZcfj8yxE",
+        title: 'Learn Git In 15 Minutes',
+        type: 'Video',
+        topic: 'Git',
+        description: "In this video, we'll go over all the important stuff you need to know to get started using Git.  We cover git add, git commit, git branch, git checkout, and git merge!",
+        isFavourite: "false"    
+    },
+    {
+        url: "https://www.valentinog.com/blog/jest/",
+        title: 'Jest Tutorial for Beginners: Getting Started With JavaScript Testing',
+        type: 'Article',
+        topic: 'Jest',
+        description: "What means testing? How to test JavaScript code with Jest? Learn the basics of testing JavaScript with this Jest tutorial for beginners!",
+        isFavourite: "false"    
+    },
+    {
+        url: "https://www.youtube.com/watch?v=8JJ101D3knE",
+        title: 'Git Tutorial for Beginners: Learn Git in 1 Hour',
+        type: 'Video',
+        topic: 'Git',
+        description: "Git tutorial for beginners - Learn Git for a career in DevOps/Software Engineering. This Git tutorial teaches you everything you need to learn Git basics.",
+        isFavourite: "false"    
+    },
+    {
+        url: "https://devhints.io/jest",
+        title: 'Jest cheatsheet',
+        type: 'Article',
+        topic: 'Jest',
+        description: "A quick overview to Jest, a test framework for Node.js. This guide targets Jest v20.",
+        isFavourite: "false"    
     }
 ]
 
